04130fa73624c73206cf5f395284685b84b0a37d
burmeai.io <athuyrain@gmail.com>
2025-08-03 16:58:30 +0630

    Update routes.js

--- scripts/router/routes.js
+++ scripts/router/routes.js
@@ -3,6 +3,7 @@ export const routes = {
     'chat': {
         template: 'chat',
         title: 'AI Chat Bot',
+        styles: ['chat'],
         init: async () => {
             const { init } = await import('../pages/chat.js');
             init();
@@ -11,6 +12,7 @@ export const routes = {
     'image': {
         template: 'image-gen',
         title: 'Image Generator',
+        styles: ['image-gen'],
         init: async () => {
             const { init } = await import('../pages/image-gen.js');
             init();
@@ -19,14 +21,16 @@ export const routes = {
     'coder': {
         template: 'coder',
         title: 'Code Generator',
+        styles: ['coder'],
         init: async () => {
-            const { init } = import('../pages/coder.js');
+            const { init } = await import('../pages/coder.js');
             init();
         }
     },
     'public-chat': {
         template: 'public-chat',
         title: 'Public Chat Groups',
+        styles: ['public-chat'],
         init: async () => {
             const { init } = await import('../pages/public-chat.js');
             init();
@@ -35,9 +39,19 @@ export const routes = {
     'friends': {
         template: 'friends-list',
         title: 'Friends List',
+        styles: ['friends-list'],
         init: async () => {
             const { init } = await import('../pages/friends-list.js');
             init();
+        }
+    },
+    'friends-chat': {
+        template: 'friends-chat',
+        title: 'Friends Chat',
+        styles: ['friends-chat'],
+        init: async (params) => {
+            const { init } = await import('../pages/friends-chat.js');
+            init(params);
         }
     }
 };
